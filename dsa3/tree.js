d7879ae3b47673bbadaff8ff76610e5d813a583c
Lena <lenafathima51@gmail.com>
2025-12-15 16:04:58 +0530

    all

--- dsa3/tree.js
+++ dsa3/tree.js
@@ -26,42 +26,164 @@
     // console.log(root)
 
 
-class TreeNode {
-  constructor(data) {
-    this.data = data;       
-    this.children = [];     
-  }
 
-  addChild(childNode) {
-    this.children.push(childNode);
-  }
+
+// class TreeNode {
+//   constructor(data) {
+//     this.data = data;       
+//     this.children = [];     
+//   }
+
+//   addChild(childNode) {
+//     this.children.push(childNode);
+//   }
+// }
+
+// const root = new TreeNode("CEO");
+// const manager1 = new TreeNode("Manager1");
+// const manager2 = new TreeNode("Manager2");
+// const emp1 = new TreeNode("Emp1");
+// const emp2 = new TreeNode("Emp2");
+// const emp3 = new TreeNode("Emp3");
+
+
+// root.addChild(manager1);
+// root.addChild(manager2);
+
+// manager1.addChild(emp1);
+// manager1.addChild(emp2);
+
+// manager2.addChild(emp3);
+
+
+// function printTree(node, indent = "") {
+//   console.log(indent + node.data);       
+//   for (let child of node.children) {
+//     printTree(child, indent + "  ");   
+//   }
+// }
+
+
+// printTree(root);
+
+
+// binary tree
+
+// class Node{
+//     constructor(value){
+//         this.value=value
+//         this.left=null
+//         this.right=null
+//     }
+// }
+
+// const root= new Node(1)
+// const value1= new Node(2)
+// const value2=new Node(3)
+// const value3=new Node(4)
+// const value4=new Node(5)
+
+
+// root.right=value1;
+// root.left=value2;
+
+
+// value1.leftt=value3
+// value1.right=value4
+
+// root;
+
+
+// function preOrder(node){
+
+//     if(!node) return
+
+//     console.log(node.value)
+
+//     preOrder(node.left)
+//     console.log(node.value)
+
+//     preOrder(node.right)
+//     console.log(node.value)
+// }
+
+
+// preOrder(root)
+
+
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
 }
 
-const root = new TreeNode("CEO");
-const manager1 = new TreeNode("Manager1");
-const manager2 = new TreeNode("Manager2");
-const emp1 = new TreeNode("Emp1");
-const emp2 = new TreeNode("Emp2");
-const emp3 = new TreeNode("Emp3");
+class BST {
+    constructor() {
+        this.root = null
+    }
 
+    insert(value) {
+        const node = new Node(value)
 
-root.addChild(manager1);
-root.addChild(manager2);
+        if (!this.root) {
+            this.root = node
+            return
+        }
 
-manager1.addChild(emp1);
-manager1.addChild(emp2);
+        let curr = this.root
 
-manager2.addChild(emp3);
+        while (true) {
+            if (value < curr.value) {
+                if (!curr.left) {
+                    curr.left = node
+                    return
+                }
+                curr = curr.left
+            } else {
+                if (!curr.right) {
+                    curr.right = node
+                    return
+                }
+                curr = curr.right
+            }
+        }
+    }
 
-
-function printTree(node, indent = "") {
-  console.log(indent + node.data);       
-  for (let child of node.children) {
-    printTree(child, indent + "  ");   
+ contain(value){
+    let curr=this.root
+    
+    while(curr){
+      if(value < curr.value){
+        curr=curr.left
+      }else if (value > curr.value){
+        curr=curr.right
+      }else{
+        return true
+      }
+    }
+    
+    return false
   }
+  
+
+
+   printInorder(node) {
+    if (!node) return
+    this.printInorder(node.left)
+    console.log(node.value)
+    this.printInorder(node.right)
 }
 
-
-printTree(root);
-
-
+}                                                           
+const tree = new BST()
+tree.insert(10)
+tree.insert(5)
+tree.insert(15)
+tree.insert(48)
+tree.insert(29)
+tree.insert(2)
+tree.insert(3)
+tree.printInorder(tree.root)
