c6016d2403745303f63d6188e54c6807b55e6ada
Lena <lenafathima51@gmail.com>
2025-12-17 11:04:25 +0530

    right

--- dsa3/heap.js
+++ dsa3/heap.js
@@ -1,70 +1,85 @@
-class MaxHeap {
-    constructor() {
-        this.heap = [];
-    } 
+class Max{
+  constructor(value){
+    this.heap=[]
+  }
+
+  
+  insert(value){
     
+    this.heap.push(value)
+    this.heapfyup()
+  }
+  
+  heapfyup(          ){
     
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp();
+    let index = this.heap.length-1
+    
+    while(index > 0){
+      let parent= Math.floor((index-1)/2)
+      
+      if(this.heap[parent] >= this.heap[index])break;
+      
+      [this.heap[parent],this.heap[index]]=[this.heap[index],this.heap[parent]]
+      
+      index=parent
     }
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        while (index > 0) {
-            let parentIndex = Math.floor((index - 1) / 2)
-            if (this.heap[parentIndex] >= this.heap[index])
-                break;
-            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
-            index = parentIndex;
-        }
-    }
-    extractMax() {
-        if (this.heap.length === 0) return null;
-        if (this.heap.length === 1) return this.heap.pop();
+    
+  }
+  
+    
+extract() {
+  if (this.heap.length === 0) return null
+  if (this.heap.length === 1) return this.heap.pop()
 
+  const max = this.heap[0]
+  this.heap[0] = this.heap.pop()
 
-        let max = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown(0);
-        return max
-    }
-    heapifyDown(index) {
-        let length = this.heap.length;
-        let left = 2 * index + 1;
-        let right = 2 * index + 2;
-        let largest = index;
+  this.heapfydown()
+  return max
+}
 
+heapfydown() {
+  let index = 0
+  let length = this.heap.length
 
-        if (left < length && this.heap[left] >= this.heap[largest]) largest = left;
-        if (right < length && this.heap[right] >= this.heap[largest]) largest = right;
-        if (index !== largest) {
-            [this.heap[largest], this.heap[index]] = [this.heap[index], this.heap[largest]];
+  while (true) {
+    let left = 2 * index + 1
+    let right = 2 * index + 2
+    let largest = index
 
-            this.heapifyDown(largest);
-        }
+    if (left < length && this.heap[left] > this.heap[largest]) {
+      largest = left
     }
 
-    front() {
+    if (right < length && this.heap[right] > this.heap[largest]) {
+      largest = right
+    }
+
+    if (largest === index) break;
+
+    [this.heap[index], this.heap[largest]] =[this.heap[largest], this.heap[index]]
+
+    index = largest
+  }
+}
+
+front() {
         return this.heap[0]
     }
     size() {
         return this.heap.length;
     }
-    print() {
-        console.log(this.heap);
-    }
 
+    
+  print(){
+    console.log(this.heap)
+  }
 }
 
-const maxHeap = new MaxHeap();
-maxHeap.insert(4)
-maxHeap.insert(8)
-maxHeap.insert(11)
-maxHeap.insert(0)
-
-maxHeap.print()
-
-maxHeap.extractMax()
-maxHeap.print()
-maxHeap.extractMax()
-maxHeap.print()
+const maxheap=new Max()
+maxheap.insert(10)
+maxheap.insert(20)
+maxheap.insert(30)
+maxheap.insert(40)
+maxheap.extract()
+maxheap.print()
